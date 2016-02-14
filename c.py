c5f087c5d387e00e7de498fbe446895972a624cb
ncornman <ncornman@stanford.edu>
2016-02-14 13:20:29 -0800

    c.py

--- c.py
+++ c.py
@@ -1,1 +1,27 @@
-c.py
+from os.path import basename, join
+
+DATADIR = 'tempdata'
+
+URL = 'http://stash.compciv.org/ssa_baby_names/ssa-babynames-nationwide-2014.txt'
+babypath = join(DATADIR, basename(URL))
+
+sortlist = []
+
+with open(babypath) as f:
+	lines = f.readlines()
+	for line in lines:
+		splitLine = line.rstrip('\n').split(',')
+		topbabynames = (splitLine[0])
+		topbabynum = (splitLine[2])
+		sortlist.append((topbabynames,topbabynum))
+
+	counter = 0
+
+	sortlist.sort(key=lambda data: len(data[0]), reverse=True)
+	for x in sortlist:
+		if int(x[1]) > 2000: 
+			if counter <= 9: 
+				counter = counter + 1
+				print (x[0].ljust(15), x[1].rjust(10)) 
+
+		
